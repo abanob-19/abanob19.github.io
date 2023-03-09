97c985a65bf6aef104191f4722b9eaccfc1dee5d
abanob-19 <abanob11@gmail.com>
2023-03-09 15:36:37 +0200

    'secondtouch'

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
+const instructorRoutes = require('./routes/instructorRoutes')
 
 // express app
 const app = express()
 
 // middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
 })
 
 // routes
-app.get('/', (req, res) => {
-  res.json({mssg: 'Welcome to the app'})
-})
+app.use('/instructor', instructorRoutes)
 
 // listen for requests
 app.listen(process.env.PORT, () => {
